fc50e7a761fca309852038d567e91bdb5526e9e5
EnokeDeelaka <enokedeelakakulathunga@gmail.com>
2022-05-01 17:26:43 +0530

    init

--- src/app/components/dashboard/inner-items/update-customer/update-customer.component.ts
+++ src/app/components/dashboard/inner-items/update-customer/update-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CustomerService} from "../../../../service/customer.service";
+import {CustomerDTO} from "../../../../dto/CustomerDTO";
 
 @Component({
   selector: 'app-update-customer',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateCustomerComponent implements OnInit {
 
-  constructor() { }
+  saveForm = new FormGroup({
+    id: new FormControl(null, [Validators.required]),
+    name: new FormControl(null, [Validators.required]),
+    address: new FormControl(null, [Validators.required]),
+    salary: new FormControl(null, [Validators.required])
+  });
+
+  constructor(private _customerService: CustomerService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  saveData() {
+    const dto = new CustomerDTO(
+      this.saveForm.get('id')?.value,
+      this.saveForm.get('name')?.value,
+      this.saveForm.get('address')?.value,
+      this.saveForm.get('salary')?.value,
+    );
+    this._customerService.saveCustomer(dto).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
